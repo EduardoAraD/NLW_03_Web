a86ac4ca63838077579258e766407b54c9bc1c06
EduardoAraD <araujocarlos893@gmail.com>
2020-10-23 14:34:19 -0300

    update EditOrphanage and PendingOrphanage

--- src/pages/PendingOrphanage.tsx
+++ src/pages/PendingOrphanage.tsx
@@ -1,10 +1,12 @@
 import React, { useState, FormEvent, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { FiXCircle, FiCheck } from 'react-icons/fi'
+
 import SideBar from "../components/Sidebar";
+import FormEditOrphanage from '../components/FormEditOrphanage';
 import api from '../services/api'
 import '../styles/pages/edit-orphanage.css';
 
-import FormEditOrphanage from '../components/FormEditOrphanage';
 
 interface Orphanage {
   name: string;
@@ -48,10 +50,18 @@ export default function PendingOrphanage() {
       setInstructions(instructions)
       setOpeningHours(opening_hours)
       setOpenOnWeekends(open_on_weekends)
+
+      var imagesUrl: string[] = [];
+
+      images.map(image => {
+        imagesUrl.push(image.url)
+      })
+      setPreviewImages(imagesUrl)
+
     })
   }, [params.id])
 
-  async function handleSubmit(event: FormEvent) {
+  async function handleSubmitConfirm(event: FormEvent) {
     event.preventDefault();
 
     const { latitude, longitude } = position;
@@ -65,15 +75,18 @@ export default function PendingOrphanage() {
     data.append('instructions', instructions)
     data.append('opening_hours', opening_hours)
     data.append('open_on_weekends', String(open_on_weekends))
+    data.append('pending', 'true')
 
     imagesFile.forEach(image => {
       data.append('images', image);
     })
 
-    //await api.post('orphanages', data)
+    await api.put(`orphanages/${params.id}`, data)
 
-    alert('Cadastro realizado com sucesso!')
+    history.push('/dashboard')
+  }
 
+  function handleSubmitRecusar(){
     history.push('/dashboard')
   }
 
@@ -82,7 +95,7 @@ export default function PendingOrphanage() {
       <SideBar />
       <div className="content-edit-page">
         <div className="title">
-          <span>Editar perfil de Orf. Esperança</span>
+          <span>Editar perfil de {name}</span>
         </div>
         <main>
           <div className="edit-form-container">
@@ -96,9 +109,16 @@ export default function PendingOrphanage() {
               images={imagesFile} setImages={setImagesFile}
               previewImages={previewImages} setPreviewImages={setPreviewImages}
             />
-            <button type="submit" onClick={handleSubmit}>
-              Confirmar
-                    </button>
+            <div className="buttons-content">
+              <button className="dont-button" onClick={handleSubmitRecusar}>
+                <FiXCircle size={24} color="rgba( 255, 255, 255, 1)" />
+                Recusar
+              </button>
+              <button className="conf-button" onClick={handleSubmitConfirm}>
+                <FiCheck size={24} color="rgba( 255, 255, 255, 1)" />
+                Confirmar
+              </button>
+            </div>
           </div>
         </main>
       </div>
