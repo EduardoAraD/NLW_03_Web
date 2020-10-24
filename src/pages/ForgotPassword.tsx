54c779ca5bb4b7ac98bf674d90a3123709412584
EduardoAraD <araujocarlos893@gmail.com>
2020-10-23 23:15:41 -0300

    update ForgotPassword

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
 import LandingLogo from '../components/LandingLogo'
+import api from '../services/api'
 
 import '../styles/pages/forgot-password.css'
 
 export default function ForgotPassword() {
+
+    const [email, setEmail] = useState('')
+
+    async function handleSubmit() {
+        console.log(email)
+        try {
+            await api.post('/forgot_password', { email })
+
+            alert('Você receberá um email para redefinir sua senha')
+
+        } catch (err) {
+            alert('Email invalido')
+        }
+    }
+
     return (
         <div id="page-forgot">
             <LandingLogo />
@@ -19,11 +35,13 @@ export default function ForgotPassword() {
                     <p>Sua redefinição de senha será enviada para o e-mail cadastrado.</p>
                     <div className="input-block">
                         <label htmlFor="email">E-mail</label>
-                        <input
-                            id="email" />
+                        <input value={email}
+                            id="email"
+                            onChange={event => setEmail(event.target.value)} />
                     </div>
                     <button className="button"
                         type="button"
+                        onClick={handleSubmit}
                     >Enviar</button>
                 </div>
             </div>
